9f19b5bec5175886dff2afa7ad624592147e47cd
Shmedi <eduard.tupy@hotmail.com>
2020-08-19 15:16:09 -0400

    Updates

---
+++ precache-manifest.cc397e6b6004ba755d5bb2ac7f0599a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2d742ad87bdf0b89a63e6b283d73e6",
+    "url": "/Moribus/index.html"
+  },
+  {
+    "revision": "bed9364ce45e490ca696",
+    "url": "/Moribus/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4219630c1048ee96baba",
+    "url": "/Moribus/static/css/main.7f16b3a3.chunk.css"
+  },
+  {
+    "revision": "bed9364ce45e490ca696",
+    "url": "/Moribus/static/js/2.b20d54ec.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/Moribus/static/js/2.b20d54ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4219630c1048ee96baba",
+    "url": "/Moribus/static/js/main.1a14eebd.chunk.js"
+  },
+  {
+    "revision": "af46d216e1ad73cb3b53",
+    "url": "/Moribus/static/js/runtime-main.21ad8030.js"
+  }
+]);
